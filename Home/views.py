bd4e3d68f33353aacc1c544754eadbff5e44d9e8
Afreen Adham <ashifay665@gmail.com>
2023-04-25 21:07:27 +0530

    first commit

---
+++ Home/views.py
@@ -0,0 +1,46 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Departments,Doctors
+from .forms import BookingForm
+
+
+# Create your views here.
+def  index(request): 
+    numbers={
+        'num1':[1,2,3,4,5,6,7,8,9,10],
+    }
+    return render(request,'index.html',numbers)
+
+def about(request):
+    return render(request,'About.html')
+
+def booking(request):
+    if request.method =="POST":
+       form = BookingForm(request.POST)
+       if form.is_valid():
+        form.save()
+
+        return render(request,'confirmation.html')
+    form = BookingForm()
+    dict_form={
+        'form':form
+    }
+    return render(request,'Booking.html',dict_form)
+
+def doctors(request):
+    dict_docs ={
+        
+        'doctors':Doctors.objects.all()
+    }
+    return render(request,'Doctors.html',dict_docs)
+
+def contact(request):
+    return render(request,'Contact.html')
+
+def department(request):
+    dict_dept={
+        'dept':Departments.objects.all()
+    }
+    return render(request,'Department.html',dict_dept)
